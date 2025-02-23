11af0ae340512e9510080d2c68454286e1376fc8
priocoder <priocoder@gmail.com>
2025-02-23 20:34:28 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Overlay function
+function openSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+}
+AOS.init({
+    duration: 1300
+});
